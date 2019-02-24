382fafa9daf5382d4d2291f6c7dbdfac73109cb2
DanielSoteloRuz <danielsoteloruz@gmail.com>
2019-02-24 09:10:46 +0100

    Completado los ajustes avanzados

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,10 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
+  fixtures :all
 end
